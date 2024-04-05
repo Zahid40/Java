6620b33a47ac7016e7555dc42efc34747b20d11f
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-04-05 23:06:09 +0530

    .

---
+++ first java project/src/com/zahid/Strings_in_java.java
@@ -0,0 +1,8 @@
+package com.zahid;
+
+public class Strings_in_java {
+    public static void main(String[] args) {
+        String name = new String("Zahid");
+        System.out.println(name);
+    }
+}
