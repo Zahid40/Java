a0e7052477b461a65062a1f5458a0622d1d6ae49
Zahid <72434947+Zahid40@users.noreply.github.com>
2024-04-30 22:59:58 +0530

    .

---
+++ first java project/src/com/zahid/Mthods_use_meme.java
@@ -0,0 +1,13 @@
+package com.zahid;
+
+public class Mthods_use_meme {
+
+    public static void HelloWorld(String s){
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+
+        HelloWorld("print");
+
+    }
+}
